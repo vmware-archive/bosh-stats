64c13dbba4de152e687bc15e755c76f538ec39e7
Derek Reeve <dreeve@pivotal.io>
2017-01-31 11:05:44 -0800

    Print out the report with tabwriter
    [#137763785] Create report for deployments/month for US-PWS 2016
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- main.go
+++ main.go
@@ -4,9 +4,44 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"text/tabwriter"
+	"time"
 
 	"github.com/pivotal-cloudops/bosh-stats/deployments"
 )
+
+func printHeader(w *tabwriter.Writer) {
+	fmt.Fprintln(w, "Deployment", "\t", "Count")
+	fmt.Fprintln(w, "--------------------", "\t", "--------------------")
+}
+
+func friendlyCalendarMonth(calendarMonth *string) string {
+	parsedMonth, err := time.Parse("2006/01", *calendarMonth)
+	friendlyCalendarMonth := parsedMonth.Format("Jan 2006")
+
+	if err != nil {
+		friendlyCalendarMonth = *calendarMonth
+	}
+
+	return friendlyCalendarMonth
+}
+
+func printResults(numberByDeployment map[string]int, calendarMonth *string) {
+	totalDeploys := 0
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+
+	printHeader(w)
+
+	for k, v := range numberByDeployment {
+		totalDeploys += v
+		fmt.Fprintln(w, k, "\t", v, "deploys")
+	}
+
+	fmt.Println()
+	fmt.Fprintln(w, "--------------------", "\t", "--------------------")
+	fmt.Fprintln(w, friendlyCalendarMonth(calendarMonth), "\t", totalDeploys, "total deploys")
+	w.Flush()
+}
 
 func main() {
 	directorURL := flag.String("directorUrl", "", "bosh director URL")
@@ -32,5 +67,6 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("Success deploys by deployment name: \n %v\n", numberByDeployment)
+
+	printResults(numberByDeployment, calendarMonth)
 }
