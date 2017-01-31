e32c15196274413c20862edeecc9b3579be8b997
Kai Xiang <kxiang@pivotal.io>
2017-01-30 17:52:50 -0800

    Print out a map of Successful deploys indexed by deployment name
    [#137763785] Create report for deployments/month for US-PWS 2016
    
    Signed-off-by: Topher Lubaway <clubaway@pivotal.io>

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	directorURL := flag.String("directorUrl", "", "bosh director URL")
 	uaaURL := flag.String("uaaUrl", "", "UAA URL")
 	uaaClientID := flag.String("uaaClientId", "", "UAA Client ID")
-	uaaClientSecret := os.Getenv("BOSH_STATS_UAA_CLIENT_SECRET")
+	uaaClientSecret := flag.String("uaaClientSecret", "", "UAA Client Secret")
 	caCert := flag.String("caCert", "", "CA Cert")
 	calendarMonth := flag.String("calendarMonth", "", "Calendar month/year YYYY/MM")
 	repaveUser := flag.String("repaveUser", "", "The username to filter out as the 'repave' user")
@@ -22,14 +22,15 @@ func main() {
 		DirectorURL:     *directorURL,
 		UaaURL:          *uaaURL,
 		UaaClientID:     *uaaClientID,
-		UaaClientSecret: uaaClientSecret,
+		UaaClientSecret: *uaaClientSecret,
 		CaCert:          *caCert,
 	}
 
-	numberOfDeploys, err := deployCounter.SuccessfulDeploys(*calendarMonth, 200, *repaveUser)
+	numberByDeployment := make(map[string]int)
+	err := deployCounter.SuccessfulDeploys(*calendarMonth, 200, *repaveUser, &numberByDeployment)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(numberOfDeploys)
+	fmt.Printf("Success deploys by deployment name: \n %v\n", numberByDeployment)
 }
